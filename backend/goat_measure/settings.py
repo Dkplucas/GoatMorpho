5293c008ab9d360320bc3fa9ab97199844baf451
lu <dossoukponganfleming@gmail.com>
2025-07-17 10:44:42 +0900

    version 0.1.4

--- backend/goat_measure/settings.py
+++ backend/goat_measure/settings.py
@@ -73,9 +73,8 @@ WSGI_APPLICATION = 'goat_measure.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
-        conn_max_age=600,
-        conn_health_checks=True,
+        default=config('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
