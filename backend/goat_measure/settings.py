6c7f81d1c49733043ff509e737d6c05953de2de4
lu <dossoukponganfleming@gmail.com>
2025-07-17 21:40:21 +0900

    version0.1.6

--- backend/goat_measure/settings.py
+++ backend/goat_measure/settings.py
@@ -16,11 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = [
-    'goatmorpho.info',
-    'www.goatmorpho.info',
-    '141.147.45.87',  # Your OCI IP (fallback)
-]
+ALLOWED_HOSTS = ['goatmorpho.info', 'www.goatmorpho.info', '141.147.45.87']
 
 # Application definition
 INSTALLED_APPS = [
